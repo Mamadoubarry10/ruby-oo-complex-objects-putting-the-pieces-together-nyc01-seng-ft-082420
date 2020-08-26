426434438faf8f4d91f7cec984daa87319d8ccee
mamadou alpha barry <mamadou.barry1105@gmail.com>
2020-08-26 02:53:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,14 @@
 # Make your shoe class here!
 class Shoe
   # attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_reader :_brand
  
-  def initialize(title)
-    @title = title
+  def initialize(brand)
+    _brand = brand
   end
  
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
+  # def turn_page
+  #   puts "Flipping the page...wow, you read fast!"
+  # end
  
 end
